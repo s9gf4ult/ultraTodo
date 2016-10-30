3dc5e71ca560e12851e527dfa6809775b073315c
Aleksey Uimanov <s9gf4ult@gmail.com>
2016-10-30 17:52:06 +0500

    change native client

--- nativeClient/nativeClient.js
+++ nativeClient/nativeClient.js
@@ -1,10 +1,23 @@
-var options =
-    [ { value: "one", label: "One" },
-      { value: "two", label: "Two" }
-    ]
+var getOptions = function(input, callback) {
+    setTimeout(function() {
+        callback(null, {
+            options: [
+                { value: 'one', label: 'One' },
+                { value: 'two', label: 'Two' }
+            ],
+            // CAREFUL! Only set this to true when there are no more options,
+            // or more specific queries will not be sent to the server.
+            complete: true
+        });
+    }, 500);
+};
 
-var printChange = function(val) {
-  console.log(val);
-}
-
-ReactDOM.render(<Select options={options} onChange={printChange} />, document.getElementById('todoClient'));
+// ReactDOM.render(<Select.Async name="form-field-name" loadOptions={getOptions} />, document.getElementById('todoClient'));
+ReactDOM.render(
+  React.createElement(
+    eval("window['Select']['Async']")
+    , { loadOptions: getOptions }
+    // , null
+  )
+  , document.getElementById('todoClient')
+);
